012f25f05de063eb718d925fc2b6074c68a98457
Alisherjon <egamberdiyevalisherjon@gmail.com>
2023-10-24 08:06:51 +0500

    js

---
+++ js/app.js
@@ -0,0 +1,112 @@
+// alert("Hello Hammaga!"); // single line
+// confirm("JS ni o'rganishga tayyormisiz?");
+// prompt("Parol kiriting: ");
+// console.log("Hammaga Salom!");
+
+// const max_life = 3;
+
+// console.log(max_life);
+
+// max_life = 4;
+
+// let age = 19;
+
+// console.log(age);
+
+// age = 20;
+
+// console.log(age);
+
+// let fName = prompt("Ismingizni kiriting: ");
+// let lName = prompt("Familiyangizni kiriting: ");
+
+// console.log(fName + " " + lName);
+
+// let input = prompt("Ismingizni kiriting: ");
+
+// console.log(input);
+
+/**
+ * Multi line comment
+ * dsf
+ * asdf
+ * asd
+ * fas
+ * df
+ * asd
+ * fas
+ * df
+ * asd
+ * fa
+ */
+
+// console.log("Nimadir \nyozu\"v")
+// harf, son, _, $
+
+// let $_person2893475983745987349583_____hjdsfjdfjkhask
+
+// data types
+// Primitive
+
+// String
+// let name = "Eshmat dkjfhksajdhfkjsadhfkja kjd kasjf kljasdl \n O'zbekiston";
+// Number
+// let age = 18;
+// let price = 999.99;
+// Boolean
+// let isGood = true;
+// let isBad = false;
+// undefined => aniqlanmagan
+// let motto = undefined;
+// console.log(motto);
+// null => bo'sh
+// let favSport = null;
+
+// console.log(favSport);
+
+// Reference => Bog'liqlik
+// Array => to'plam
+// let skills = ["HTML", "CSS", "BS", "SASS", "AOS", "JS"];
+// console.log(skills);
+
+// Object
+// let person = {
+//   name: "Eshmat",
+//   email: "eshmat@gmail.com",
+//   age: 32,
+//   isMarried: true,
+//   isLearning: true,
+//   skills: [
+//     "HTML",
+//     "CSS",
+//     {
+//       name: "js",
+//       level: "beginner",
+//     },
+//   ],
+// };
+
+// console.log(person);
+
+/* 
+  Ism: {ism},
+  Familiya: {familiya},
+  yoshi: {age}da
+*/
+
+// let lName = "Toshmatov";
+
+// console.log("Toliq ismi Eshmat " + lName + "yoshi");
+
+// let person2 = {
+//   address: {
+//     street: "Navoiy",
+//     city: "Toshkent",
+//     district: "Shayxontohur",
+//   },
+// };
+
+let age = {};
+
+console.log(typeof age);
+console.log(Array.isArray(age));
